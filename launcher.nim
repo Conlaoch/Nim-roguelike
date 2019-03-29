4bb8c0b68047c65734df66c07f5c5210cbd378e8
Zireael07 <zi@mail.com>
2019-03-29 19:45:23 +0100

    fix: update launcher for new page path

--- launcher.nim
+++ launcher.nim
@@ -7,4 +7,4 @@ let dir = os.getCurrentDir()
 
 ## to fit 800x600 canvas without scrollbars
 ## we use file:// protocol to open a local site
-open("Minimal webview example", "file:///"&dir&"/index.html", 850, 650, true)
+open("Minimal webview example", "file:///"&dir&"/docs/index.html", 850, 650, true)
