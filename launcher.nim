360b7da3b4368f788cbe577bbbc5f620f3ae885e
Zireael07 <zi@mail.com>
2019-04-22 10:54:10 +0200

    fix: bigger launcher window size to fit keypad

--- launcher.nim
+++ launcher.nim
@@ -5,6 +5,6 @@ let dir = os.getCurrentDir()
 
 ##echo(dir)
 
-## to fit 800x600 canvas without scrollbars
+## to fit 800x600 canvas and keypad without scrollbars
 ## we use file:// protocol to open a local site
-open("Minimal webview example", "file:///"&dir&"/docs/index.html", 850, 650, true)
+open("Minimal webview example", "file:///"&dir&"/docs/index.html", 850, 850, true)
