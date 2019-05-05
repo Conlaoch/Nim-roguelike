6268df2854117f8b6eaee8c66af14ade2bb4367d
Zireael07 <zi@mail.com>
2019-05-05 13:28:42 +0200

    new: update webview url for http serving

--- launcher.nim
+++ launcher.nim
@@ -6,5 +6,10 @@ let dir = os.getCurrentDir()
 ##echo(dir)
 
 ## to fit 800x600 canvas and keypad without scrollbars
-## we use file:// protocol to open a local site
-open("Minimal webview example", "file:///"&dir&"/docs/index.html", 850, 850, true)
+## we serve the site because otherwise we can't request data files
+## this function from server module is blocking, we can't really do anything while this runs
+##defaultServe()
+
+## YOU NEED TO RUN ./server first (e.g. from a terminal) (or python3 -m http.serve if you have python)
+open("Minimal webview example", "http://localhost:8000", 850, 850, true)
+#open("Minimal webview example", "file:///"&dir&"/docs/index.html", 850, 850, true)
